67a2630c14dcef4eb69a564c3babb03827d40a8f
Felipe Lima <felipe_lima10@icloud.com>
2021-09-03 14:13:59 -0300

    Updates

---
+++ precache-manifest.84a7aae4dabe58e5a7a79583712f752d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135e108bc5fedc246c5237453c7af78a",
+    "url": "/project-wallet-redux/index.html"
+  },
+  {
+    "revision": "a5f2ab1c8da6b821528b",
+    "url": "/project-wallet-redux/static/css/main.3e51d52f.chunk.css"
+  },
+  {
+    "revision": "3a5af2dae826733d14ce",
+    "url": "/project-wallet-redux/static/js/2.5729d50c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-wallet-redux/static/js/2.5729d50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f2ab1c8da6b821528b",
+    "url": "/project-wallet-redux/static/js/main.35f1e39f.chunk.js"
+  },
+  {
+    "revision": "359b949e230cc662ede5",
+    "url": "/project-wallet-redux/static/js/runtime-main.284ab107.js"
+  },
+  {
+    "revision": "c063d5ab1c5af73840b6461a64b86bdf",
+    "url": "/project-wallet-redux/static/media/Login.c063d5ab.png"
+  },
+  {
+    "revision": "d46ea51dac68d18bee972f0122718e72",
+    "url": "/project-wallet-redux/static/media/arrow.d46ea51d.svg"
+  },
+  {
+    "revision": "cbfce9ba53107b2a68337ed967d4d27f",
+    "url": "/project-wallet-redux/static/media/delete.cbfce9ba.svg"
+  },
+  {
+    "revision": "dbff935c6a032aeb73b561580645301e",
+    "url": "/project-wallet-redux/static/media/edit.dbff935c.svg"
+  },
+  {
+    "revision": "b588dd38d6ee8bee2bf4d04fff6a529a",
+    "url": "/project-wallet-redux/static/media/vector.b588dd38.svg"
+  }
+]);
